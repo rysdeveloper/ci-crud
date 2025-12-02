8d0e0be50cd3c31e5cc0969b1e8ad85e6b1dd29d
Yuvraj Singh Rathore <rathoreyuvrajsingh2001@gmail.com>
2025-12-02 18:55:44 +0530

    created product model

---
+++ app/Models/ProductModel.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+class ProductModel extends Model
+{
+    protected $table = 'products';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['name', 'description', 'price', 'stock'];
+    protected $useTimestamps = true;
+    protected $useSoftDeletes = true;
+    protected $deletedField = 'deleted_at';
+    protected $returnType = 'array';
+
+    protected $validationRules = [
+        'name'        => 'required|min_length[3]|max_length[255]',
+        'price'       => 'required|decimal',
+        'stock'       => 'permit_empty|is_natural',
+    ];
+
+    protected $validationMessages = [
+        'name' => [
+            'required' => 'Please enter a product name.',
+        ],
+        'price' => [
+            'decimal' => 'Enter a valid price (e.g., 99.99).',
+        ]
+    ];
+}
