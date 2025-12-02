ea03994d07c888b77f74023936f82ee52e1342ef
Yuvraj Singh Rathore <rathoreyuvrajsingh2001@gmail.com>
2025-12-02 20:00:33 +0530

    created product routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/products', 'ProductController::index');
+$routes->get('/products/create', 'ProductController::create');
+$routes->post('/products/store', 'ProductController::store');
+$routes->get('/products/edit/(:num)', 'ProductController::edit/$1');
+$routes->post('/products/update/(:num)', 'ProductController::update/$1');
+$routes->get('/products/delete/(:num)', 'ProductController::delete/$1');
