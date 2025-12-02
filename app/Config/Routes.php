eb0c1daec80f9c1e71cd7b681ccff791d9563bb2
Yuvraj Singh Rathore <rathoreyuvrajsingh2001@gmail.com>
2025-12-02 20:02:54 +0530

    created base layout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'ProductController::index');
 $routes->get('/products', 'ProductController::index');
 $routes->get('/products/create', 'ProductController::create');
 $routes->post('/products/store', 'ProductController::store');
